09cf0ee61da5bba49745ebb1cd52ac27cd98c4ad
jaruiz59 <47218326+jaruiz59@users.noreply.github.com>
2019-05-17 14:31:16 -0400

    Add files via upload
    dewh2idb

---
+++ Project-master/The-House-An-Interactive-Fiction-master/main.js
@@ -0,0 +1,38 @@
+\// Create the contents of your room here.
+alert("Loading main.js!"); //Don't change this line
+player = new Player()
+
+//Create your objects
+let hallway = new Room("dusty hallway", "clouds of dust kick up with every step.");
+let vase = new Item("vase", "made of blue glass, chipped on top. Filled with a dark liquid.");
+let room = new Room("dark room", "It is dark");
+let note = new Item("found a note,the note is torn, Damage,spots of blood ");
+let knife = new Item("rusty,old dried up blood ");
+let bedroom = new Room("pealing off paint,broken old chair ");
+let dinningRoom = new room('old beroken plates,old rotten fruits');
+let basement = new room ('old big boxes, dead rats in the corner')
+let mirror = new Item ()
+let Gun = new Item ()
+let
+
+// Put them in their spots
+
+hallway.addItem(vase);
+dark.addItem(room);
+dark.addroom(note);
+dark.addItem(knife);
+dark.addroom(bedroom);
+dark.addItem(dinningRoom);
+dark.addItem(basement);
+dark.addItem(dinningRoom);
+dark.addItem(mirror);
+dark.addItem(Gun);
+dark.addItem();
+hallway.addItem(dark);
+
+
+
+
+
+player.location = hallway;
+player.location.enter();
